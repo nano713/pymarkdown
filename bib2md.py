b655958d49a109e1c1489b6a5a579b065a79d32c
nano713 <daichikozawa@gmail.com>
2024-04-16 21:14:59 +0900

    Add some comments for clarity and also separate the creation of the markdown entry into a separate function for better readability and maintainability.

--- bib2md.py
+++ bib2md.py
@@ -1,29 +1,36 @@
 import bibtexparser
 
+
+def create_md_entry(i, entry):
+    """Create a markdown entry for a given bibtex entry."""
+    authors = entry['author'].replace(' and ', ', ')
+    title = entry['title']
+    journal = entry['journal']
+    volume = entry.get('volume', '')
+    pages = entry.get('pages', '')
+    year = entry.get('year', '')
+    doi = entry.get('doi', '')
+    url = entry.get('url', '')
+
+    doi_link = f'{doi}' if doi else ''
+    md_entry = f'{i + 1}. {authors}. **{title}.** *{journal}*, {volume}, {pages} ({year}). {doi_link}\n\n'
+
+    return md_entry
+
+
 def convert_bib_to_md(bib_filename, md_filename):
+    """Convert a .bib file to a markdown file."""
     with open(bib_filename) as bibtex_file:
         bib_database = bibtexparser.load(bibtex_file)
 
     # Sort the entries by year in descending order
     sorted_entries = sorted(bib_database.entries, key=lambda entry: entry.get('year', ''), reverse=True)
 
-    md_bibliography = ""
-    for i, entry in enumerate(sorted_entries):
-        authors = entry['author'].replace(' and ', ', ')
-        title = entry['title']
-        journal = entry['journal']
-        volume = entry.get('volume', '')
-        pages = entry.get('pages', '')
-        year = entry.get('year', '')
-        doi = entry.get('doi', '')
-        url = entry.get('url', '')
-
-        doi_link = f'[{doi}]({url})' if doi else ''
-        md_entry = f'{i+1}. {authors}. **{title}.** *{journal}*, {volume}, {pages} ({year}). {doi_link}\n\n'
-        md_bibliography += md_entry
+    md_bibliography = "".join(create_md_entry(i, entry) for i, entry in enumerate(sorted_entries))
 
     with open(md_filename, 'w') as md_file:
         md_file.write(md_bibliography)
+
 
 # Usage
 bib_filename = 'your_bib_file.bib'  # replace with your .bib file
